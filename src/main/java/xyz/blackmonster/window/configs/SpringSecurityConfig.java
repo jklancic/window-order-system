4561f12d91b8c81113c428df6e67c251b8a66fb5
jklancic <jernej.klancic@appdirect.com>
2018-10-08 15:13:48 -0600

    Fixed 403 response for calling REST api(s)

--- src/main/java/xyz/blackmonster/window/configs/SpringSecurityConfig.java
+++ src/main/java/xyz/blackmonster/window/configs/SpringSecurityConfig.java
@@ -30,6 +30,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+			.csrf().disable()
 			.authorizeRequests()
 				.antMatchers("/", "/assets/**", "/api/order/calculate", "/api/order/**").permitAll()
 				.antMatchers("/admin/**").hasRole("ADMIN")
